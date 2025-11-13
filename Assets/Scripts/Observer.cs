7bae272eca95a419752e18cb70391c1f88f8e57b
ctuelho <duartulio@gmail.com>
2025-11-13 16:39:47 -0300

    working on observers, obsmanager and their effects

--- Assets/Scripts/Observer.cs
+++ Assets/Scripts/Observer.cs
@@ -6,11 +6,17 @@ public class Observer : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDrag
     public Slot CurrentSlot { get; set; }
     private Vector3 _startPosition;
     private Transform _startParent;
+    private Collider2D _col;
 
     public int range = 0;
     public DecayType decay = DecayType.DoesNotDecay;
 
     public enum DecayType { DoesNotDecay = 0, Spread = 1, VerySlow = 2, Slow = 3, Medium = 4, Fast = 5, VeryFast = 6 }
+
+    private void Awake()
+    {
+        _col = GetComponent<Collider2D>();
+    }
 
     public void OnBeginDrag(PointerEventData eventData)
     {
@@ -23,6 +29,7 @@ public class Observer : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDrag
         }
 
         transform.SetParent(null);
+        _col.enabled = false;
     }
 
     public void OnDrag(PointerEventData eventData)
@@ -35,6 +42,7 @@ public class Observer : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDrag
     public void OnEndDrag(PointerEventData eventData)
     {
         GameObject droppedOn = eventData.pointerCurrentRaycast.gameObject;
+        _col.enabled = true;
 
         if (droppedOn != null)
         {
@@ -42,7 +50,6 @@ public class Observer : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDrag
 
             if (targetSlot != null)
             {
-                // O Observer interage com o Slot, e o Slot chama o Manager.
                 targetSlot.HandleObserverDrop(this);
                 return;
             }
